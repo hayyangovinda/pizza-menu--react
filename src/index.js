854b498fcd27cb32a854d7473cae14444ac25fdc
hayyan <hayyan@chb-technologies.ch>
2025-08-04 17:04:30 +0400

    add jsx

--- src/index.js
+++ src/index.js
@@ -49,9 +49,41 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Header />
+      <Menu />
+
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer>
+      {new Date().toLocaleTimeString()}{" "}
+      {isOpen ? "We're currently open!" : "Sorry, we're closed!"}
+    </footer>
   );
 }
 
